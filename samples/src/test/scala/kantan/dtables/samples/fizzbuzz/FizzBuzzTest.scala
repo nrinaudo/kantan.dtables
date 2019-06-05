c32d67e81fe041a3042a98133e8ab2efc375ee1c
Nicolas Rinaudo <nicolas@nrinaudo.com>
2019-06-05 14:57:39 +0200

    Simplify the FizzBuzz example

--- samples/src/test/scala/kantan/dtables/samples/fizzbuzz/FizzBuzzTest.scala
+++ samples/src/test/scala/kantan/dtables/samples/fizzbuzz/FizzBuzzTest.scala
@@ -18,54 +18,42 @@ package kantan.dtables
 package samples
 package fizzbuzz
 
+import annotation.tailrec
 import kantan.dtables.properties._
 import kantan.dtables.scalatest._
 import org.scalacheck._
 import org.scalatest._
 
-/** Enumerates all possible output behaviours. */
-sealed abstract class FizzBuzzOutput extends Product with Serializable
-object FizzBuzzOutput {
-  case object Input    extends FizzBuzzOutput
-  case object Fizz     extends FizzBuzzOutput
-  case object Buzz     extends FizzBuzzOutput
-  case object FizzBuzz extends FizzBuzzOutput
-
-  implicit val outputDecoder: EntryDecoder[FizzBuzzOutput] = EntryDecoder.fromUnsafe {
-    case "N"        => FizzBuzzOutput.Input
-    case "Fizz"     => FizzBuzzOutput.Fizz
-    case "Buzz"     => FizzBuzzOutput.Buzz
-    case "FizzBuzz" => FizzBuzzOutput.FizzBuzz
-  }
-}
-
-/** System under test: FizzBuzz implementation. */
-object FizzBuzz extends Function1[Int, String] {
-  override def apply(i: Int): String =
+/** Actual FizzBuzz test. */
+class FizzBuzzTest extends FunSuite with DTables {
+  // - Simple FizzBuzz implementation ----------------------------------------------------------------------------------
+  // -------------------------------------------------------------------------------------------------------------------
+  val sut: Int => String = i =>
     if(i % 3 == 0) {
       if(i % 5 == 0) "FizzBuzz"
       else "Fizz"
     }
     else if(i % 5 == 0) "Buzz"
     else i.toString
-}
 
-/** Actual FizzBuzz test. */
-class FizzBuzzTest extends FunSuite with DTables {
+  // - Decision table decoding -----------------------------------------------------------------------------------------
+  // -------------------------------------------------------------------------------------------------------------------
+
+  // Divides `i` by `div` until the result is no longer divisible by `div`.
+  // This is necessary to make sure that "seed" values aren't divisible by 3 or 5.
+  @tailrec
+  private def nonDivisible(i: Int, div: Int): Int =
+    if(i % div == 0) nonDivisible(i / div, div)
+    else i
+
   // Decodes a set of conditions into a Gen[Int] whose output map these conditions.
   implicit val genDecoder: ConditionsDecoder[Int] = ConditionsDecoder.decoder("n % 3", "n % 5") {
     (mod3: Boolean, mod5: Boolean) =>
-      // Divides `i` by `div` until the result is no longer divisible by `div`.
-      // This is necessary to make sure that "seed" values aren't divisible by 3 or 5.
-      def simplify(i: Int, div: Int): Int =
-        if(i % div == 0) simplify(i / div, div)
-        else i
-
       // Generates integers that are:
       // - divisible by 3 if mod3 is true
       // - divisible by 5 is mod5 is true
       Gen.choose(1, 100).map { i =>
-        val seed = simplify(simplify(i, 3), 5)
+        val seed = nonDivisible(nonDivisible(i, 3), 5)
 
         if(mod3) {
           if(mod5) seed * 3 * 5
@@ -77,20 +65,20 @@ class FizzBuzzTest extends FunSuite with DTables {
   }
 
   // Decodes outcomes into expected behaviours.
-  implicit val outcomeDecoder: OutcomeDecoder[FizzBuzzOutput] = OutcomeDecoder.fromEntry("Output")
+  implicit val outcomeDecoder: OutcomeDecoder[Int => String] = OutcomeDecoder.fromEntry[String]("Output").emap {
+    case "N"                                => DecodeResult.success(_.toString)
+    case s @ ("Fizz" | "Buzz" | "FizzBuzz") => DecodeResult.success(_ => s)
+    case s                                  => DecodeResult.error(s"Not a valid fizzbuzz output: $s")
+  }
+
+  // - Actual test -----------------------------------------------------------------------------------------------------
+  // -------------------------------------------------------------------------------------------------------------------
 
   // Loads the fizzbuzz specs.
   val spec = csv.load(getClass.getResource("/fizzbuzz.csv")).getOrElse(sys.error("Failed to load fizzbuz specs"))
 
   // For all test cases, makes sure output matches expectations.
-  checkAll("FizzBuzz", spec) { (input: Int, expected: FizzBuzzOutput) =>
-    val result = FizzBuzz(input)
-
-    expected match {
-      case FizzBuzzOutput.Fizz     => result == "Fizz"
-      case FizzBuzzOutput.FizzBuzz => result == "FizzBuzz"
-      case FizzBuzzOutput.Buzz     => result == "Buzz"
-      case FizzBuzzOutput.Input    => result == input.toString
-    }
+  checkAll("FizzBuzz", spec) { (input: Int, expected: Int => String) =>
+    sut(input) == expected(input)
   }
 }
